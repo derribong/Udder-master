ce041b38039b9cff84e7fb8daa0858c1b601b23f
Walter <WhsHome@imac2016-2.local>
2018-10-13 18:00:00 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Udder
+Tutorial- LondonAppBrewery
+
+
+This is a test for a tutorial on Udemy - LondonAppBrewery - Angela Yu
---
+++ Udder/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by admin on 06/11/2017.
+//  Copyright © 2017 Four40Four. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
+    }
+    
+    override func viewLayoutMarginsDidChange() {
+        
+    }
+    
+}
+
